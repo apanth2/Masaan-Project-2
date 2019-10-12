ccec5096c1f8759e9200e2068c7ac9da3ccf5f2b
Amit Panthi <47650855+apanth2@users.noreply.github.com>
2019-10-12 04:54:22 -0600

    Alpha almost completed

---
+++ Assets/laptop_on_script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laptop_on_script : MonoBehaviour
+{
+
+    public GameObject laptopoff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        laptopoff.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/light_script.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light_script : MonoBehaviour
+{
+
+    public GameObject light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        light.SetActive(true);
+    }
+
+
+        void Awake()
+    {
+
+
+
+        
+
+
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
